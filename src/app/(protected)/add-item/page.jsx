548230363e7507d2a3164064adf2ca2a23e5985a
Md Abdul Ahad <abd.me7@gmail.com>
2026-01-18 00:52:34 +0600

    Project Setup

---
+++ src/app/(protected)/add-item/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddItems = () => {
+    return (
+        <div>
+            This is Add Items page
+        </div>
+    );
+};
+
+export default AddItems;
---
+++ src/app/(public)/items/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemDetails = () => {
+    return (
+        <div>
+            This is Item Details
+        </div>
+    );
+};
+
+export default ItemDetails;
